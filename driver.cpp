54db43d3b16ec27ecbf969ebf51916c0796e0f07
Gerhard Serton <gerhardserton@gmail.com>
2019-05-12 23:18:25 +0200

    Created a driver class

--- driver.cpp
+++ driver.cpp
@@ -5,47 +5,360 @@
 using namespace std;
 int main(int argc, char * argv[])
 {
-	int sampleRate = (int)(argv[2]);
-	int bitCount = (int)(argv[4]) / 8;
+	int sampleRate = stoi(argv[2]);
+	int bitCount = stoi(argv[4]) / 8;
 	string bitCountString = argv[4];
 	int highValue = ((int)(pow(2.0,(double)bitCount)) / 2) - 1;
-	int channels = (int)(argv[6]);
+	int channels = stoi(argv[6]);
 	string channelsString = "MONO";
 	if (channels == 2)
 	{
 		channelsString = "STEREO";
 	}
-	string outName = "out"
+	string outName = "out";
 	string argument;
-	string soundFile1;
 	if (argv[7] == "-o")
 	{
 		outName = argv[8];
 		argument = argv[9];
-		soundFile1 = argv[10];
 	}
 	else
 	{
 		argument = argv[7];
-		soundFile1 = argv[8];
 	}
 	string outNameFinal;
-	outNameFinal << outName << "_" << sampleRate << "_" << bitCountString << "_" << channelsString << ".raw";
+	outNameFinal = outName + "_" + to_string(sampleRate) + "_" + bitCountString + "_" + channelsString + ".raw";
 	cout << "Outputting to: " << outNameFinal << "\n";
 
+
 	if (argument == "-add")
-	{}
+	{
+		string soundFile1 = argv[10];
+		string soundFile2 = argv[11];
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+				Sound<int8_t> sound2(sampleRate, soundFile2, outNameFinal, 1, 1, 127);
+
+				Sound<int8_t> sound3 = sound1 + sound2;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+				Sound<int16_t> sound2(sampleRate, soundFile2, outNameFinal, 1, 2, 32767);
+
+				Sound<int16_t> sound3 = sound1 + sound2;
+				sound3.saveSound();
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+				Sound<pair<int8_t, int8_t>> sound2(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				Sound<pair<int8_t, int8_t>> sound3 = sound1 + sound2;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+				Sound<pair<int16_t, int16_t>> sound2(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				Sound<pair<int16_t, int16_t>> sound3 = sound1 + sound2;
+				sound3.saveSound();
+			}
+		}
+	}
+
 	else if (argument == "-cut")
-	{}
-	else if (argument == "-radd")
-	{}
+	{
+		string soundFile1 = argv[12];
+		pair<int, int> cut = make_pair(stoi(argv[10]), stoi(argv[11]));
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+
+				Sound<int8_t> sound3 = sound1 ^ cut;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+
+				Sound<int16_t> sound3 = sound1 ^ cut;
+				sound3.saveSound();
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				Sound<pair<int8_t, int8_t>> sound3 = sound1 ^ cut;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				Sound<pair<int16_t, int16_t>> sound3 = sound1 ^ cut;
+				sound3.saveSound();
+			}
+		}
+	}
+	else if (argument == "-radd") //make it work for 2 seperate cuts.
+	{
+		string soundFile1 = argv[14];
+		string soundFile2 = argv[15];
+		int start1 = stoi(argv[10]);
+		int end1 = stoi(argv[11]);
+		int start2 = stoi(argv[12]);
+		int end2 = stoi(argv[13]);
+
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+				Sound<int8_t> sound2(sampleRate, soundFile2, outNameFinal, 1, 1, 127);
+
+				Sound<int8_t> sound3 = sound1.rangedAdd(sound2, start1, end1, start2, end2);
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+				Sound<int16_t> sound2(sampleRate, soundFile2, outNameFinal, 1, 2, 32767);
+
+				Sound<int16_t> sound3 = sound1.rangedAdd(sound2, start1, end1, start2, end2);
+				sound3.saveSound();
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+				Sound<pair<int8_t, int8_t>> sound2(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				Sound<pair<int8_t, int8_t>> sound3 = sound1.rangedAdd(sound2, start1, end1, start2, end2);
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+				Sound<pair<int16_t, int16_t>> sound2(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				Sound<pair<int16_t, int16_t>> sound3 = sound1.rangedAdd(sound2, start1, end1, start2, end2);
+				sound3.saveSound();
+			}
+		}
+	}
 	else if (argument == "-cat")
-	{}
+	{
+		string soundFile1 = argv[10];
+		string soundFile2 = argv[11];
+
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+				Sound<int8_t> sound2(sampleRate, soundFile2, outNameFinal, 1, 1, 127);
+
+				Sound<int8_t> sound3 = sound1 | sound2;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+				Sound<int16_t> sound2(sampleRate, soundFile2, outNameFinal, 1, 2, 32767);
+
+				Sound<int16_t> sound3 = sound1 | sound2;
+				sound3.saveSound();
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+				Sound<pair<int8_t, int8_t>> sound2(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				Sound<pair<int8_t, int8_t>> sound3 = sound1 | sound2;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+				Sound<pair<int16_t, int16_t>> sound2(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				Sound<pair<int16_t, int16_t>> sound3 = sound1 | sound2;
+				sound3.saveSound();
+			}
+		}
+	}
 	else if (argument == "-v")
-	{}
+	{
+		string soundFile1 = argv[12];
+		pair<float, float> volume = make_pair(stoi(argv[10]), stoi(argv[11]));
+
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+
+				Sound<int8_t> sound3 = sound1 * volume.first;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+
+				Sound<int16_t> sound3 = sound1 * volume.first;
+				sound3.saveSound();
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				Sound<pair<int8_t, int8_t>> sound3 = sound1 * volume;
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				Sound<pair<int16_t, int16_t>> sound3 = sound1 * volume;
+				sound3.saveSound();
+			}
+		}
+	}
 	else if (argument == "-rev")
-	{}
+	{
+		string soundFile1 = argv[10];
+
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+
+				Sound<int8_t> sound3 = sound1.reverse();
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+
+				Sound<int16_t> sound3 = sound1.reverse();
+				sound3.saveSound();
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				Sound<pair<int8_t, int8_t>> sound3 = sound1.reverse();
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				Sound<pair<int16_t, int16_t>> sound3 = sound1.reverse();
+				sound3.saveSound();
+			}
+		}
+	}
 	else if (argument == "-rms")
-	{}
+	{
+		string soundFile1 = argv[8];
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+
+				cout << sound1.rms() << "\n";
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+
+				cout << sound1.rms() << "\n";
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				pair<float, float> rms = sound1.rms();
+				cout << rms.first << " " << rms.second << "\n";
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				pair<float, float> rms = sound1.rms();
+				cout << rms.first << " " << rms.second << "\n";
+			}
+		}
+	}
 	else if (argument == "-norm")
+	{
+		string soundFile1 = argv[12];
+		pair<float, float> desiredRms = make_pair(stoi(argv[10]), stoi(argv[11]));
+
+		if (channels == 1)
+		{
+			if (bitCount == 1)
+			{
+				Sound<int8_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 1, 127);
+
+				Sound<int8_t> sound3 = sound1.normalisation(desiredRms.first);
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<int16_t> sound1(sampleRate, soundFile1, outNameFinal, 1, 2, 32767);
+
+				Sound<int16_t> sound3 = sound1.normalisation(desiredRms.first);
+				sound3.saveSound();
+			}
+		}
+		else
+		{
+			if (bitCount == 1)
+			{
+				Sound<pair<int8_t, int8_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 1, 127);
+
+				Sound<pair<int8_t, int8_t>> sound3 = sound1.normalisation(desiredRms);
+				sound3.saveSound();
+			}
+			else
+			{
+				Sound<pair<int16_t, int16_t>> sound1(sampleRate, soundFile1, outNameFinal, 2, 2, 32767);
+
+				Sound<pair<int16_t, int16_t>> sound3 = sound1.normalisation(desiredRms);
+				sound3.saveSound();
+			}
+		}
+	}
 }
