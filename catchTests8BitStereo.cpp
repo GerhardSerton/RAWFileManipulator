c45bb1dd8dc9e5b09e6f4a0efda6d4e3bcdd5d4a
Gerhard Serton <gerhardserton@gmail.com>
2019-05-12 20:04:24 +0200

    Added functionality for stereo and associated tests

---
+++ catchTests8BitStereo.cpp
@@ -0,0 +1,378 @@
+#define CATCH_CONFIG_MAIN
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
+#include <cmath>
+#include <utility>
+#include "catch.hpp"
+#include "sound.h"
+
+TEST_CASE("Constructor: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+
+  test.saveSound();
+
+  std::ifstream loadFile ("8bitstereo.raw", std::ios::in | std::ios::binary);
+  loadFile.seekg(0, loadFile.end);
+  int fileByteSize = loadFile.tellg();
+  loadFile.seekg(0, loadFile.beg);
+  std::int8_t buffer [fileByteSize];
+  loadFile.read((char *)buffer, fileByteSize);
+  loadFile.close();
+
+  std::ifstream loadFile2 ("8bitstereotest.raw", std::ios::in | std::ios::binary);
+  loadFile2.seekg(0, loadFile2.end);
+  int fileByteSize2 = loadFile2.tellg();
+  loadFile2.seekg(0, loadFile2.beg);
+  std::int8_t buffer2 [fileByteSize2];
+  loadFile2.read((char *)buffer2, fileByteSize2);
+  loadFile2.close();
+
+  REQUIRE(fileByteSize == fileByteSize2);
+
+  for (int i = 0; i < fileByteSize; ++i)
+  {
+    //std::cout << i << ": Buffer 1: " << buffer[i] << " Buffer 2: " << buffer2[i] << "\n";
+    //std::cout << "fileByteSize: " << fileByteSize << "\n";
+    REQUIRE(buffer[i] == buffer2[i]);
+  }
+
+}
+
+TEST_CASE("Copy Semantics: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+
+  Sound<std::pair<std::int8_t, std::int8_t>> test2(test);
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> one = test.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> two = test2.returnVector();
+
+  REQUIRE(one == two);
+
+  test = test2;
+  std::vector<std::pair<std::int8_t, std::int8_t>> three = test.returnVector();
+
+  REQUIRE(two == three);
+
+  Sound<std::pair<std::int8_t, std::int8_t>> test3(Sound<std::pair<std::int8_t, std::int8_t>>(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127));
+  std::vector<std::pair<std::int8_t, std::int8_t>> four = test3.returnVector();
+
+  REQUIRE(four == one);
+
+  test3 = Sound<std::pair<std::int8_t, std::int8_t>>(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  std::vector<std::pair<std::int8_t, std::int8_t>> five = test3.returnVector();
+
+  REQUIRE(five == one);
+}
+
+TEST_CASE("+ operator: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  Sound<std::pair<std::int8_t, std::int8_t>> test2(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+
+  Sound<std::pair<std::int8_t, std::int8_t>> result = test1 + test2;
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> resultvec = result.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> addResult;
+  auto test1i = test1vec.begin();
+
+  while (test1i != test1vec.end())
+  {
+    std::pair<std::int8_t, std::int8_t> t1p = *test1i;
+    int leftInt = (int)t1p.first * 2;
+    int rightInt = (int)t1p.second * 2;
+    if (leftInt > 127)
+    {
+      leftInt = 127;
+    }
+    else if (leftInt < -128)
+    {
+      leftInt < -128;
+    }
+
+    if (rightInt > 127)
+    {
+      rightInt = 127;
+    }
+    else if (rightInt < -128)
+    {
+      rightInt < -128;
+    }
+    std::int8_t left = leftInt;
+    std::int8_t right = rightInt;
+    std::pair<std::int8_t, std::int8_t> rp = std::make_pair(left, right);
+
+    addResult.push_back(rp);
+
+    ++test1i;
+  }
+
+  REQUIRE(addResult == resultvec);
+}
+
+TEST_CASE("| Operator: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  Sound<std::pair<std::int8_t, std::int8_t>> test2(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+
+  Sound<std::pair<std::int8_t, std::int8_t>> result = test1 | test2;
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> resultvec = result.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test2vec = test2.returnVector();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> catResult;
+  auto test1i = test1vec.begin();
+  auto test2i = test2vec.begin();
+
+  while (test1i != test1vec.end())
+  {
+    catResult.push_back(*test1i);
+    ++test1i;
+  }
+
+  while (test2i != test2vec.end())
+  {
+    catResult.push_back(*test2i);
+    ++test2i;
+  }
+
+  REQUIRE(catResult == resultvec);
+}
+
+TEST_CASE("* Operator: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  float volume = 1.5f;
+
+  Sound<std::pair<std::int8_t, std::int8_t>> result = test1 * volume;
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> resultvec = result.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> volResult;
+  auto test1i = test1vec.begin();
+  while (test1i != test1vec.end())
+  {
+    std::pair<std::int8_t, std::int8_t> t1p = *test1i;
+
+    float lf = (float)(t1p.first) * volume;
+    float rf = (float)(t1p.second) * volume;
+
+    if (lf > 127.0f)
+    {
+      lf = 127.0f;
+    }
+    else if (lf < -128.0f)
+    {
+      lf = -128.0f;
+    }
+
+    if (rf > 127.0f)
+    {
+      rf = 127.0f;
+    }
+    else if (rf < -128.0f)
+    {
+      rf = -128.0f;
+    }
+
+    std::int8_t left = (int)lf;
+    std::int8_t right = (int)rf;
+    std::pair<std::int8_t, std::int8_t> presult = std::make_pair(left, right);
+    volResult.push_back(presult);
+    ++test1i;
+  }
+
+  REQUIRE(volResult == resultvec);
+
+
+}
+
+TEST_CASE("^ Operator: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  std::pair<int, int> cut = std::make_pair(20, 80);
+
+  Sound<std::pair<std::int8_t, std::int8_t>> result = test1 ^ cut;
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> resultvec = result.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> cutResult;
+  auto test1i = test1vec.begin() + cut.first;
+  while (test1i != test1vec.begin() + cut.second)
+  {
+    cutResult.push_back(*test1i);
+    ++test1i;
+  }
+
+  REQUIRE(cutResult == resultvec);
+}
+
+TEST_CASE("rangedAdd Method: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  Sound<std::pair<std::int8_t, std::int8_t>> test2(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  int start = 20;
+  int end = 80;
+
+  Sound<std::pair<std::int8_t, std::int8_t>> result = test1.rangedAdd(test2, 20, 80);
+  std::vector<std::pair<std::int8_t, std::int8_t>> resultvec = result.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test2vec = test2.returnVector();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> radResult;
+  auto test1i = test1vec.begin() + 20;
+  auto test2i = test2vec.begin() + 20;
+
+  while (test1i != test1vec.begin() + 80)
+  {
+    std::pair<std::int8_t, std::int8_t> t1p = *test1i;
+    std::pair<std::int8_t, std::int8_t> t2p = *test2i;
+    int leftInt = (int)(t1p.first) + (int)(t2p.first);
+    int rightInt = (int)(t1p.second) + (int)(t2p.second);
+    if (leftInt > 127)
+    {
+      leftInt = 127;
+    }
+    else if (leftInt < -128)
+    {
+      leftInt < -128;
+    }
+
+    if (rightInt > 127)
+    {
+      rightInt = 127;
+    }
+    else if (rightInt < -128)
+    {
+      rightInt < -128;
+    }
+
+    std::int8_t left = leftInt;
+    std::int8_t right = rightInt;
+
+    std::pair<std::int8_t, std::int8_t> resultpair = std::make_pair(left, right);
+    radResult.push_back(resultpair);
+
+    ++test1i;
+    ++test2i;
+  }
+
+  auto finali = test1vec.begin() + 20;
+  auto radi = radResult.begin();
+  while (radi != radResult.end())
+  {
+    *finali = *radi;
+    ++finali;
+    ++radi;
+  }
+
+  REQUIRE(test1vec == resultvec);
+}
+
+TEST_CASE("reverse Method: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+
+  Sound<std::pair<std::int8_t, std::int8_t>> result = test1.reverse();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> resultvec = result.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> revResult;
+  auto test1i = test1vec.end() - 1;
+  while (test1i != test1vec.begin() - 1)
+  {
+    revResult.push_back(*test1i);
+    --test1i;
+  }
+
+  REQUIRE(revResult == resultvec);
+}
+
+TEST_CASE("rms Method: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+
+  std::pair<float, float> result = test1.rms();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+
+  auto test1i = test1vec.begin();
+  float lefttotal = 0.0f;
+  float righttotal = 0.0f;
+  while (test1i != test1vec.end())
+  {
+    std::pair<std::int8_t, std::int8_t> p = *test1i;
+    lefttotal += (float)(p.first) * (float)(p.first);
+    righttotal += (float)(p.second) * (float)(p.second);
+    ++test1i;
+  }
+  float savgLeft = sqrt(lefttotal / (float)(test1vec.size()));
+  float savgRight = sqrt(righttotal / (float)(test1vec.size()));
+
+  REQUIRE(savgLeft == result.first);
+  REQUIRE(savgRight == result.second);
+}
+
+TEST_CASE("normalisation Method: 8 Bit Stereo")
+{
+  Sound<std::pair<std::int8_t, std::int8_t>> test1(44100, "8bitstereo.raw", "8bitstereotest.raw", 2, 1, 127);
+  float desiredRms = 1.5;
+  std::pair<float, float> currentRms = test1.rms();
+
+  Sound<std::pair<std::int8_t, std::int8_t>> result = test1.normalisation(desiredRms);
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> resultvec = result.returnVector();
+  std::vector<std::pair<std::int8_t, std::int8_t>> test1vec = test1.returnVector();
+
+  std::vector<std::pair<std::int8_t, std::int8_t>> norResult;
+  auto test1i = test1vec.begin();
+
+  while (test1i != test1vec.end())
+  {
+    std::pair<std::int8_t, std::int8_t> p = *test1i;
+    float leftFloat = (float)(p.first) * (desiredRms / currentRms.first);
+    float rightFloat = (float)(p.second) * (desiredRms / currentRms.second);
+
+    int left = (int)(leftFloat);
+    int right = (int)(rightFloat);
+
+    if (left > 127)
+    {
+      left = 127;
+    }
+    else if (left < -128)
+    {
+      left = -128;
+    }
+
+    if (right > 127)
+    {
+      right = 127;
+    }
+    else if (right < -128)
+    {
+      right = -128;
+    }
+
+    std::int8_t l = left;
+    std::int8_t r = right;
+
+    std::pair<int8_t, int8_t> final = std::make_pair(l, r);
+
+    norResult.push_back(final);
+
+    ++test1i;
+  }
+
+  REQUIRE(norResult == resultvec);
+
+}
--- sound.h
+++ sound.h
@@ -10,6 +10,7 @@
 #include <utility>
 #include <algorithm>
 #include <numeric>
+#include <cmath>
 
 template<typename T> class Sound
 {
@@ -73,7 +74,7 @@ public:
       NumberOfSamples = fileByteSize / (sizeOfIntN * channels);
       T buffer [NumberOfSamples];
 
-      loadFile.read((char *)buffer, NumberOfSamples);
+      loadFile.read((char *)buffer, fileByteSize);
       loadFile.close();
 
       std::unique_ptr<std::vector<T>> temp (new std::vector<T>);
@@ -100,7 +101,7 @@ public:
     {
       buffer[i] = soundstream->at(i);
     }
-    outFile.write((char *)buffer, NumberOfSamples);
+    outFile.write((char *)buffer, fileByteSize);
     outFile.close();
   }
 
@@ -228,15 +229,22 @@ public:
       return Sound<T>(result, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
 
     }
-    std::vector<std::int8_t> & returnVector()
+    std::vector<T> & returnVector()
     {
       return *soundstream;
     }
 
     float rms()
     {
-      float result = std::accumulate(soundstream->begin(), soundstream->end(), 0.0f);
-      result = result / (float)(fileByteSize);
+      std::vector<float> squared;
+      std::transform(soundstream->begin(), soundstream->end(), back_inserter(squared), [] (T x){
+        float y = (float)(x);
+        float result = (y * y);
+        return result;
+      });
+      float result = std::accumulate(squared.begin(), squared.end(), 0.0f);
+      result = result / (float)(NumberOfSamples);
+      result = sqrt(result);
       return result;
     }
 
@@ -263,10 +271,334 @@ public:
       });
 
       return Sound<T>(result, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
-      
+
     }
 
 
+};
+
+template<typename T> class Sound<std::pair<T, T>>
+{
+private:
+  std::unique_ptr<std::vector<std::pair<T, T>>> soundstream;
+  int sampleRate;
+  int sizeOfIntN;
+  int maximumSizeOfIntN;
+  int channels;
+  int NumberOfSamples; //Need a function to calculate this
+  int fileByteSize; //Need a function to calculate this
+  std::string soundFile;
+  std::string outputFile;
+public:
+
+  //--------------------Constructors--------------------
+
+  Sound(std::vector<std::pair<T, T>> soundStream, int sampleRate, std::string soundFile, std::string outputFile, int channels, int sizeOfIntN, int maximumSizeOfIntN): sampleRate(sampleRate), soundFile(soundFile), outputFile(outputFile), channels(channels), sizeOfIntN(sizeOfIntN), maximumSizeOfIntN(maximumSizeOfIntN)
+  {
+    std::unique_ptr<std::vector<std::pair<T, T>>> temp (new std::vector<std::pair<T, T>>);
+    int count = 0;
+    for (auto i = soundStream.begin(); i != soundStream.end(); ++i)
+    {
+      temp->push_back(*i);
+      ++count;
+    }
+    NumberOfSamples = count;
+    fileByteSize = NumberOfSamples * (sizeOfIntN * channels);
+    soundstream = std::move(temp);
+  }
+
+  Sound(int sampleRate, std::string soundFile, std::string outputFile, int channels, int sizeOfIntN, int maximumSizeOfIntN): sampleRate(sampleRate), soundFile(soundFile), outputFile(outputFile), channels(channels), sizeOfIntN(sizeOfIntN), maximumSizeOfIntN(maximumSizeOfIntN)
+  {
+    importSound();
+  }
+
+  Sound(const Sound & rhs): sampleRate(rhs.sampleRate), soundFile(rhs.soundFile), outputFile(rhs.outputFile), channels(rhs.channels), sizeOfIntN(rhs.sizeOfIntN), NumberOfSamples(rhs.NumberOfSamples), fileByteSize(rhs.fileByteSize), maximumSizeOfIntN(rhs.maximumSizeOfIntN)
+  {
+    std::unique_ptr<std::vector<std::pair<T, T>>> temp (new std::vector<std::pair<T, T>>);
+    for (auto i = rhs.soundstream->begin(); i != rhs.soundstream->end(); ++i)
+    {
+      temp->push_back(*i);
+    }
+    soundstream = std::move(temp);
+  }
+
+  Sound(Sound && rhs): sampleRate(std::move(rhs.sampleRate)), soundFile(std::move(rhs.soundFile)), outputFile(std::move(rhs.outputFile)), channels(std::move(rhs.channels)), sizeOfIntN(std::move(sizeOfIntN)), NumberOfSamples(std::move(rhs.NumberOfSamples)), fileByteSize(std::move(rhs.fileByteSize)), maximumSizeOfIntN(std::move(rhs.maximumSizeOfIntN))
+  {
+    soundstream = std::move(rhs.soundstream);
+  }
+
+  //--------------------File import and output--------------------
+  void importSound()
+  {
+    std::ifstream loadFile (soundFile, std::ios::in | std::ios::binary);
+    if (loadFile)
+    {
+      loadFile.seekg(0, loadFile.end);
+      fileByteSize = loadFile.tellg();
+      loadFile.seekg(0, loadFile.beg);
+      //Remember: int8_t = 1 byte,  int 16_t = 2 bytes
+      NumberOfSamples = fileByteSize / (sizeOfIntN * channels);
+      T buffer [fileByteSize/sizeOfIntN];
+
+      loadFile.read((char *)buffer, fileByteSize);
+      loadFile.close();
+
+      std::unique_ptr<std::vector<std::pair<T, T>>> temp (new std::vector<std::pair<T, T>>);
+
+      for (int i = 0; i < fileByteSize/sizeOfIntN; i = i+2)
+      {
+        std::pair<T, T> tpair = std::make_pair(buffer[i], buffer[i+1]);
+        temp->push_back(tpair);
+      }
+
+      soundstream = std::move(temp);
+
+    }
+    else
+    {
+      std::cout << "ERROR: FILE NOT FOUND\n";
+    }
+  }
+
+  void saveSound()
+  {
+    std::ofstream outFile (outputFile, std::ios::out | std::ios::binary);
+    T buffer [fileByteSize/sizeOfIntN];
+    int bufferPosition = 0;
+    for (auto i = soundstream->begin(); i != soundstream->end(); ++i)
+    {
+      std::pair<T, T> t = *i;
+      buffer[bufferPosition] = t.first;
+      buffer[bufferPosition + 1] = t.second;
+      bufferPosition = bufferPosition + 2;
+    }
+    outFile.write((char *)buffer, fileByteSize);
+    outFile.close();
+  }
+
+  //--------------------Operators--------------------
+
+  Sound & operator = (const Sound & rhs)
+  {
+    sampleRate = rhs.sampleRate;
+    soundFile = rhs.soundFile;
+    outputFile = rhs.outputFile;
+    channels = rhs.channels;
+    sizeOfIntN = rhs.sizeOfIntN;
+    NumberOfSamples = rhs.NumberOfSamples;
+    fileByteSize = rhs.fileByteSize;
+    maximumSizeOfIntN = rhs.maximumSizeOfIntN;
+
+    std::unique_ptr<std::vector<std::pair<T, T>>> temp (new std::vector<std::pair<T, T>>);
+    for (auto i = rhs.soundstream->begin(); i != rhs.soundstream->end(); ++i)
+    {
+      temp->push_back(*i);
+    }
+    soundstream = std::move(temp);
+
+    return *this;
+  }
+
+  Sound & operator = (Sound && rhs)
+  {
+    sampleRate = std::move(rhs.sampleRate);
+    soundFile = std::move(rhs.soundFile);
+    outputFile = std::move(rhs.outputFile);
+    channels = std::move(rhs.channels);
+    sizeOfIntN = std::move(rhs.sizeOfIntN);
+    NumberOfSamples = std::move(rhs.NumberOfSamples);
+    fileByteSize = std::move(rhs.fileByteSize);
+    maximumSizeOfIntN = std::move(rhs.maximumSizeOfIntN);
+    soundstream = std::move(rhs.soundstream);
+
+    return *this;
+  }
+
+  Sound operator + (Sound rhs) //add amplitudes
+  {
+    std::vector<std::pair<T, T>> temp;
+    auto rhsi = rhs.soundstream->begin();
+    auto thisi = soundstream->begin();
+    while (thisi != soundstream->end())
+    {
+      std::pair<T, T> rhsp = *rhsi;
+      std::pair<T, T> thisp = *thisi;
+      int op1 = (int)(rhsp.first) + (int)(thisp.first);
+      if (op1 > maximumSizeOfIntN)
+      {
+        op1 = maximumSizeOfIntN;
+      }
+      else if (op1 < (0 - maximumSizeOfIntN - 1))
+      {
+        op1 = (0 - maximumSizeOfIntN - 1);
+      }
+
+      int op2 = (int)(rhsp.second) + (int)(thisp.second);
+      if (op2 > maximumSizeOfIntN)
+      {
+        op2 = maximumSizeOfIntN;
+      }
+      else if (op2 < (0 - maximumSizeOfIntN - 1))
+      {
+        op2 = (0 - maximumSizeOfIntN - 1);
+      }
+
+      T left = op1;
+      T right = op2;
+      std::pair<T, T> final = std::make_pair(left, right);
+      temp.push_back(final);
+
+      ++thisi;
+      ++rhsi;
+    }
+
+    return Sound<std::pair<T, T>>(temp, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
+  }
+
+  Sound operator | (Sound rhs) //concatenate
+  {
+    std::vector<std::pair<T, T>> result;
+    std::copy(soundstream->begin(), soundstream->end(), back_inserter(result));
+    std::copy(rhs.soundstream->begin(), rhs.soundstream->end(), back_inserter(result));
+
+    return Sound<std::pair<T, T>>(result, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
+  }
+
+  Sound operator * (float rhs) //volume factor
+  {
+    std::vector<std::pair<T, T>> result;
+    int max = maximumSizeOfIntN;
+    std::transform(soundstream->begin(), soundstream->end(), back_inserter(result), [rhs, max] (std::pair<T, T> x) {
+      int left = (int)((int)(x.first) * rhs);
+      int right = (int)((int)(x.second) * rhs);
+
+      if (left > max)
+      {
+        left = max;
+      }
+      else if (left < (0 - max - 1))
+      {
+        left = (0 - max - 1);
+      }
+
+      if (right > max)
+      {
+        right = max;
+      }
+      else if (right < (0 - max - 1))
+      {
+        right = (0 - max - 1);
+      }
+      T l = left;
+      T r = right;
+      std::pair<T, T> final = std::make_pair(l, r);
+      return final;
+    });
+    return Sound<std::pair<T, T>>(result, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
+  }
+
+  Sound operator ^ (std::pair<int, int> rhs) //cut
+  {
+    std::vector<std::pair<T, T>> result;
+    std::copy(soundstream->begin() + rhs.first, soundstream->begin() + rhs.second, back_inserter(result));
+
+    return Sound<std::pair<T, T>>(result, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
+  }
+
+  //--------------------Methods--------------------
+
+  std::vector<std::pair<T, T>> & returnVector()
+  {
+    return *soundstream;
+  }
+
+  Sound rangedAdd(Sound rhs, int start, int end)
+  {
+    std::pair<int, int> cut = std::make_pair(start, end);
+    Sound<std::pair<T, T>> cutResult = (rhs ^ cut) + (*this ^ cut);
+    Sound<std::pair<T, T>> finalResult(*this);
+    std::copy(cutResult.soundstream->begin(), cutResult.soundstream->end(), finalResult.soundstream->begin() + cut.first);
+
+    return finalResult;
+  }
+
+  Sound reverse()
+  {
+    std::vector<std::pair<T, T>> result = *soundstream;
+    std::reverse(result.begin(), result.end());
+
+    return Sound<std::pair<T, T>>(result, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
+
+  }
+
+  std::pair<float, float> rms()
+  {
+    std::vector<float> leftsquared;
+    std::vector<float> rightsquared;
+
+    std::transform(soundstream->begin(), soundstream->end(), back_inserter(leftsquared), [] (std::pair<T, T> x){
+      float left = (float)(x.first);
+      left = left * left;
+
+      return left;
+    });
+
+    std::transform(soundstream->begin(), soundstream->end(), back_inserter(rightsquared), [] (std::pair<T, T> x){
+      float right = (float)(x.second);
+      right = right * right;
+
+      return right;
+    });
+
+    float resultLeft = std::accumulate(leftsquared.begin(), leftsquared.end(), 0.0f);
+    float resultRight = std::accumulate(rightsquared.begin(), rightsquared.end(), 0.0f);
+    resultLeft = sqrt(resultLeft / (float)(NumberOfSamples));
+    resultRight = sqrt(resultRight / (float)(NumberOfSamples));
+    return std::make_pair(resultLeft, resultRight);
+  }
+
+  Sound normalisation(float desiredRms)
+  {
+    std::pair<float, float> currentRms = rms();
+    int max = maximumSizeOfIntN;
+    std::vector<std::pair<T, T>> result;
+
+    std::transform(soundstream->begin(), soundstream->end(), back_inserter(result), [desiredRms, currentRms, max] (std::pair<T, T> x) {
+      float leftFloat = (float)(x.first) * (desiredRms / currentRms.first);
+      float rightFloat = (float)(x.second) * (desiredRms / currentRms.second);
+
+      int left = (int)(leftFloat);
+      int right = (int)(rightFloat);
+
+      if (left > max)
+      {
+        left = max;
+      }
+      else if (left < (0 - max - 1))
+      {
+        left = (0 - max - 1);
+      }
+
+      if (right > max)
+      {
+        right = max;
+      }
+      else if (right < (0 - max - 1))
+      {
+        right = (0 - max - 1);
+      }
+
+      T l = left;
+      T r = right;
+
+      std::pair<T, T> final = std::make_pair(l, r);
+
+      return final;
+    });
+
+    return Sound<std::pair<T, T>>(result, sampleRate, soundFile, outputFile, channels, sizeOfIntN, maximumSizeOfIntN);
+
+  }
 };
 
 
